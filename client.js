afb9ee046f00465f21440e9d47325774a9e1292d
Kara Bayse <karabayse@gmail.com>
2018-03-26 14:33:08 -0500

    initial commit

---
+++ client.js
@@ -0,0 +1,11 @@
+console.log('in client.js');
+
+// Reverse a string
+// example:  reverseString('hello') === 'olleh'
+
+function reverseString(str) {
+  const strArr = str.split('');
+  stringArray.reverse();
+  console.log(strArr);
+  return strArr.join('');
+}
