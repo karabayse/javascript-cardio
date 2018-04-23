3fd42e6322c33b3be23b3a6d84cc2ec97360eec3
Kara Bayse <karabayse@gmail.com>
2018-04-23 14:19:09 -0500

    capitalize the first letter of every word

--- client.js
+++ client.js
@@ -113,3 +113,43 @@ console.log(output9);
 
 
 // CAPITALIZE LETTERS
+// Return a string with the first letter of every word capitalized
+
+function capitalizeLetters(str) {
+  const strArr = str.toLowerCase().split(' ');
+
+  for (let i = 0; i < strArr.length; i++) {
+    strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+  }
+  return strArr.join(' ');
+}
+
+const output10 = capitalizeLetters('i love javascript');
+console.log(output10);
+
+
+function capitalizeLetters2(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    console.log(word);
+    return word[0].toUpperCase() + word.substr(1);
+  })
+  .join(' ');
+}
+
+const output11 = capitalizeLetters2('I love javaScript');
+console.log(output11);
+
+
+function capitalizeLetters3(str) {
+  // regular expression:  between / /
+  // boundary:  \b
+  // range:  [a-z]
+  // g:  global
+  // i:  case insensitive 
+  return str.replace(/\b[a-z]/gi, function(char) {
+    return char.toUpperCase;
+  });
+}
+
+const output12 = capitalizeLetters3('i Love javascript');
+console.log(output12);
